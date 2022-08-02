4c5c880e113aa6bf33ce5ecd81598d79c453e7e0
ShriKrushna Bhagwat <krish30jan@gmail.com>
2022-08-02 20:03:36 +0530

    Update whatsapp_noti.py

--- whatsapp_noti.py
+++ whatsapp_noti.py
@@ -3,8 +3,8 @@
 
 ## Send WhatsApp Order Notification
 from datetime import date
-TWILIO_ACOUNT_SID='ACd963c39bbfa6be628fe2333199df3af6'
-TWILIO_AUTH_TOKEN='5f44fb1df80a74c027dd76252d77080e'
+TWILIO_ACOUNT_SID='Your SID Key'
+TWILIO_AUTH_TOKEN='Your Auth Token'
 
 from twilio.rest import Client
 
@@ -21,7 +21,7 @@ client = Client(TWILIO_ACOUNT_SID, TWILIO_AUTH_TOKEN)
 # this is the Twilio sandbox testing number
 from_whatsapp_number='whatsapp:+14155238886'
 # replace this number with your own WhatsApp Messaging number
-to_whatsapp_number='whatsapp:+918742881915'
+to_whatsapp_number='whatsapp:+91<10 digit number onto whom you want to send message>'
 
 client.messages.create(body='1 order at '+dt_string,
                        from_=from_whatsapp_number,
